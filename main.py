0e2f4150b29121a00219919a1eb0524a00a5d867
Aaron Henson <aaron.henson96@gmail.com>
2024-11-26 21:50:19 -0800

    Finished code, could not figure out how to make it both calculate continuously and store previous calculations in time.

--- main.py
+++ main.py
@@ -5,17 +5,16 @@
 # All numbers are rounded to the nearest whole number, no decimals
 # A calculator.txt file will be created to store all values until
 
-file = open("calculator.txt", "w")
-file.write("0")
+file = open("calculator.txt", "w") #create calculator.txt file
 file.close()
 
-def main():
-    while True:
+def main(): #create the calculator
+    while True:    # create the menu for user to choose which operation they want to do
         print("1. Add")
         print("2. Subtract")
         print("3. Multiply")
         print("4. Divide")
-        print("5. Reset")
+        print("5. View Calculations")
         print("6. Exit")
         choice = input("Enter your choice of operation: ")
         if choice == "1":
@@ -27,61 +26,59 @@ def main():
         elif choice == "4":
             divide()
         elif choice == "5":
-            reset()
+            view_calculations()
         elif choice == "6":
-            break
-        else:
+            break               # exits the loop
+        else:                   # user entered invalid option
             print("Invalid choice")
 
 
 def add():
-    num = int(input("Enter a number: "))
-    file = open("calculator.txt", "r")
-    value = int(file.read())
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number"))
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")    # will print out the entire function, such as: 5 + 5 = 10
+    file = open("calculator.txt", "a")
+    file.write(f"{num1} + {num2} = {result}\n") # write the calculation to the calculator.txt file
     file.close()
-    file = open("calculator.txt", "w")
-    value += num
-    file.write(str(value))
-    file.close()
-    print("Result:", value)
 
 def subtract():
-    num = int(input("Enter a number: "))
-    file = open("calculator.txt", "r")
-    value = int(file.read())
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number"))
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")    # will print out the entire function, such as: 20 - 10 = 10
+    file = open("calculator.txt", "a")
+    file.write(f"{num1} - {num2} = {result}\n") # write the calculation to the calculator.txt file
     file.close()
-    file = open("calculator.txt", "w")
-    value -= num
-    file.write(str(value))
-    file.close()
-    print("Result:", value)
 
 def multiply():
-    num = int(input("Enter a number: "))
-    file = open("calculator.txt", "r")
-    value = int(file.read())
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number"))
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")    # will print out the entire function, such as: 5 * 5 = 25
+    file = open("calculator.txt", "a")
+    file.write(f"{num1} * {num2} = {result}\n") # write the calculation to the calculator.txt file
     file.close()
-    file = open("calculator.txt", "w")
-    value *= num
-    file.write(str(value))
-    file.close()
-    print("Result:", value)
 
 def divide():
-    num = int(input("Enter a number: "))
-    file = open("calculator.txt", "r")
-    value = int(file.read())
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number"))
+    result = num1 / num2
+    print(f"{num1} / {num2} = {result}")    # will print out the entire function, such as: 100 / 10 = 10
+    file = open("calculator.txt", "a")
+    file.write(f"{num1} / {num2} = {result}\n") # write the calculation to the calculator.txt file
     file.close()
-    file = open("calculator.txt", "w")
-    value //= num
-    file.write(str(value))
-    file.close()
-    print("Result:", value)
 
-def reset():
-    file = open("calculator.txt", "w")
-    file.write("0")
+def view_calculations():
+    file = open("calculator.txt", "r")
+    calculations = file.readlines() # read all lines in the calculator.txt file
     file.close()
-    print("Result: 0")
+    for c in calculations:
+        print(c.strip())
 
 main()
+
+# thanks to some help from:
+# https://www.geeksforgeeks.org/make-simple-calculator-using-python/
+# https://medium.com/@muhamadafrizal5/building-a-simple-calculator-with-python-f676132d3e10
+# https://www.geeksforgeeks.org/python-output-using-print-function/
